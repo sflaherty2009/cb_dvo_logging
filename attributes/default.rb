ea5561e2735030c843ac5fce39857086bd8f15d1
Ray Crawford <ray_crawford@trekbikes.com>
2017-07-07 11:59:08 -0400

    Move SumoLogic attributes into the 'dvo_user' namespace.

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,4 +1,4 @@
-default['sumologic']['version'] = '19.182-44'
-default['sumologic']['checksum'] = '2d6390ca9cbe2370e728e42ffb892e900ee31e1f3fd8aa82d1d6714731165638'
-default['sumologic']['url'] = 'https://collectors.sumologic.com/rest/download/rpm/64'
-default['sumologic']['package_name'] = "SumoCollector #{default['sumologic']['version']}"
+default['dvo_user']['sumologic']['version'] = '19.182-44'
+default['dvo_user']['sumologic']['checksum'] = '2d6390ca9cbe2370e728e42ffb892e900ee31e1f3fd8aa82d1d6714731165638'
+default['dvo_user']['sumologic']['url'] = 'https://collectors.sumologic.com/rest/download/rpm/64'
+default['dvo_user']['sumologic']['package_name'] = "SumoCollector #{default['sumologic']['version']}"
