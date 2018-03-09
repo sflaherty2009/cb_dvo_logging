479e1fb0661da5eeef4795bf65bcd1056a002233
Ray Crawford <ray_crawford@trekbikes.com>
2018-03-08 20:02:31 -0500

    Add freegeoip logging to sources template

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'ray_crawford@trekbikes.com'
 license 'All Rights Reserved'
 description 'Installs/Configures cb_dvo_logging'
 long_description 'Installs/Configures cb_dvo_logging'
-version '2.0.11'
+version '2.0.12'
 
 source_url 'https://bitbucket.org/trekbikes/cb_dvo_logging'
 issues_url 'https://bitbucket.org/trekbikes/cb_dvo_logging/issues?status=new&status=open'
