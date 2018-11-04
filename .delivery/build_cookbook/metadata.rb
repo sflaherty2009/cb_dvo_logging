fc9bd8464c99bcb2176252656e121903a3e5009b
Matthew Oleksowicz <matthew_oleksowicz@trekbikes.com>
2018-11-04 08:17:35 -0600

    Added data drives to delivery Terraform templates.

--- .delivery/build_cookbook/metadata.rb
+++ .delivery/build_cookbook/metadata.rb
@@ -2,7 +2,7 @@ name 'build_cookbook'
 maintainer 'DevOps'
 maintainer_email 'devops@trekbikes.com'
 license 'all_rights'
-version '0.1.3'
+version '0.1.4'
 chef_version '>= 12.1' if respond_to?(:chef_version)
 source_url 'https://bitbucket.org/trekbikes/build_cookbook'
 issues_url 'https://bitbucket.org/trekbikes/build_cookbook/issues?status=new&status=open'
