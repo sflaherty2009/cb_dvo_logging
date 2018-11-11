2acb38d3221801a39d0cd08b9d95641cbb581c98
Matthew Oleksowicz <matthew_oleksowicz@trekbikes.com>
2018-11-11 08:44:34 -0600

    Fixed sumologic collector validation during functional testing.

--- .delivery/build_cookbook/recipes/tests_functional.rb
+++ .delivery/build_cookbook/recipes/tests_functional.rb
@@ -9,11 +9,15 @@ vault_data = get_chef_vault_data
 ruby_block 'check_sumologic_collector' do
   block do
     response = JSON.parse(Chef::HTTP.new('https://api.us2.sumologic.com').get('/api/v1/collectors', 'AUTHORIZATION' => "Basic #{Base64.strict_encode64("#{vault_data['sumologic_accessid']}:#{vault_data['sumologic_accesskey']}")}", 'Accept' => 'application/json', 'Content-Type' => 'application/json'))
-
+    nodes = shell_out("knife search node 'environment:acceptance-trek-trek-bikes-#{workflow_change_project}-master' --attribute name | grep 'name:' | awk '{print $2}'").stdout.chomp.split(/\n/)
+    collectors = []
     found = false
+
     response['collectors'].each do |collector|
-      found = true if collector['name'] == 'azl-chef-deli' && collector['alive']
+      collectors << collector['name'] if collector['alive']
     end
+
+    found = true if (nodes - collectors).empty?
 
     raise 'Sumologic collector is not working.' unless found
   end
