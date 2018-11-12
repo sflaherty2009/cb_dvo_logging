3a0958e7198e9f466cb59b1b4590bb03c65462c2
Matthew Oleksowicz <matthew_oleksowicz@trekbikes.com>
2018-11-11 22:26:31 -0600

    Restricted cb_dvo_terraform and functional tests to only run in acceptance.

--- .delivery/build_cookbook/recipes/provision.rb
+++ .delivery/build_cookbook/recipes/provision.rb
@@ -4,4 +4,4 @@
 #
 # Copyright:: 2018, Trek Bicycle Corporation, All Rights Reserved.
 include_recipe 'delivery-truck::provision'
-include_recipe 'cb_dvo_terraform::acceptance'
+include_recipe 'cb_dvo_terraform::acceptance' if workflow_stage?('acceptance')
