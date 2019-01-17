9ca1c9d589a6fc923fd2c779ee2be07ebfb5b0f2
Matthew Oleksowicz <matthew_oleksowicz@trekbikes.com>
2019-01-17 14:50:34 -0600

    Fixed cb_infra_acceptance recipe inclusion.

--- .delivery/build_cookbook/recipes/provision.rb
+++ .delivery/build_cookbook/recipes/provision.rb
@@ -4,4 +4,4 @@
 #
 # Copyright:: 2018, Trek Bicycle Corporation, All Rights Reserved.
 include_recipe 'delivery-truck::provision'
-include_recipe 'cb_infra_acceptance' if workflow_stage?('acceptance')
+include_recipe 'cb_infra_acceptance::run' if workflow_stage?('acceptance')
